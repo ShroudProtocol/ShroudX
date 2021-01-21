a3c3b68372eeb69cae1b622085e50975434a8905
Panther107th <edgar.keek@gmail.com>
2021-01-21 21:03:18 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90014;
+static const int PROTOCOL_VERSION = 90015;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 90011;
@@ -19,10 +19,10 @@ static const int INIT_PROTO_VERSION = 90011;
 static const int GETHEADERS_VERSION = 90010;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION - 2;
+static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION - 1;
 
 //! disconnect from all older peers after Shroudnode payment HF
-static const int MIN_PEER_PROTO_VERSION_AFTER_UPDATE = PROTOCOL_VERSION;
+static const int MIN_PEER_PROTO_VERSION_AFTER_UPDATE = PROTOCOL_VERSION - 1;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
